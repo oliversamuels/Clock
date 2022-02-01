dcf5a8ecfc275937bfe4c17bba22e9f7c1ae398f
oliversamuels <amoskevinsamuel@yahoo.com>
2022-02-01 14:20:11 +0100

    Defining some colours

--- Clock/Clock.py
+++ Clock/Clock.py
@@ -7,3 +7,7 @@ pygame.init()
 screen = pygame.display.set_mode((WIDTH, HEIGHT))
 pygame.display.set_caption("Analog Clock")
 clock = pygame.time.Clock()
+
+WHITE = (255, 255, 255)
+BLACK = (0, 0, 0)
+RED = (255, 0, 0)
